7a829fd4cacf9a8def1458b2fd80293d3069bb56
Djordje Stanojevic <djolezmo@gmail.com>
2016-03-26 11:42:49 +0100

    inheritance practice

--- app/test.js
+++ app/test.js
@@ -8,5 +8,26 @@ require([], function(){
         this.name = name;
         this.age = age;
         this.jmbg = jmbg;
+    };
+    
+    Osoba.prototype.getName = function(){
+        return this.name;
     }
+
+    window.Osoba = Osoba;
+
+    function Student(index, faculty){
+        this.index = index;
+        this.faculty = faculty;
+       //Osoba.call(this, name, age, jmbg);
+    };
+    
+    
+
+    s = new Student("2006/433", "Elektrotehnicki");
+    s.prototype = new Osoba("Djordje", 28, 87728682738989);
+
+    window.s = s;
+
+
 });
