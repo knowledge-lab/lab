67619c10c676628d58ec5c1ebe40cb6e671c28ad
Tatjana <vasiljevictatjana@yahoo.com>
2016-03-26 11:15:06 +0100

    ptototype vezba

--- app/main.js
+++ app/main.js
@@ -1,6 +1,26 @@
-require([
+/*require([
     'tool',
     'konstante'
 ], function (tool, konstante) {
     tool(konstante.A);
+});*/
+
+requre([], funtion(){
+   function A(name){
+        this.name = name;
+    }
+
+    A.prototype .name = 'default';
+    A.prototype.getName = function() {
+        return this.name;
+    };
+
+    window.a1 = new A('Zika');
+    window.a2 = new A();
+
+    function B(name,age){
+        this.age = age;
+        A.call(this, name);
+    }
+
 });
