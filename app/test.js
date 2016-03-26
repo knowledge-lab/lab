85210c873b9327b3f4804e6671a3b87a8989cffd
Djordje Stanojevic <djolezmo@gmail.com>
2016-03-26 11:15:41 +0100

    test file added

---
+++ app/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 3/26/2016.
+ */
+
+require([], function(){
+
+    function Osoba(name, age, jmbg){
+        this.name = name;
+        this.age = age;
+        this.jmbg = jmbg;
+    }
+});
