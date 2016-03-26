75f71f32209e78f7f7d56550f30da11770b0ef02
vlajkod <vlajko.dragicevic@gmail.com>
2016-03-26 11:36:52 +0100

    Prvi commit

--- app/main.js
+++ app/main.js
@@ -1,41 +1,79 @@
 require([], function () {
-   function A(name) {
-       if (typeof name === 'string') {
-           this.name = name;
-       }
-   }
+   // function A(name) {
+   //     if (typeof name === 'string') {
+   //         this.name = name;
+   //     }
+   // }
+   //
+   //  A.prototype.getName = function () {
+   //      return this.name;
+   //  };
+   //
+   //  A.prototype.name = 'podrazumevano ime';
+   //
+   //  window.a1 = new A('djura');
+   //  window.a2 = new A();
+   //
+   //
+   //  function B (age) {
+   //      if (typeof age === 'number') {
+   //          this.age = age;
+   //      }
+   //  }
+   //
+   //  B.prototype = new A();
+   //  B.prototype.constructor = B;
+   //
+   //  B.prototype.age = -1;
+   //
+   //  B.prototype.getAge = function () {
+   //      return this.age;
+   //  };
 
-    A.prototype.getName = function () {
-        return this.name;
-    };
 
-    A.prototype.name = 'podrazumevano ime';
-
-    window.a1 = new A('djura');
-    window.a2 = new A();
-
-
-    function B (age) {
-        if (typeof age === 'number') {
-            this.age = age;
+    function Osoba(name, prezime){
+        if(typeof name === 'string' && typeof prezime === 'string'){
+            this.name = name;
+            this.prezime = prezime;
         }
     }
 
-    B.prototype = new A();
-    B.prototype.constructor = B;
+    Osoba.prototype.name = 'n';
+    Osoba.prototype.prezime = 'n';
 
-    B.prototype.age = -1;
+    Osoba.prototype.fullName = function () {
+        return this.name + " " + this.prezime;
+    }
 
-    B.prototype.getAge = function () {
-        return this.age;
-    };
+    function OsobaOsobine(godiste, mestoRodjenja){
+        if(typeof godiste === 'number' && typeof mestoRodjenja === 'string'){
+            this.godiste = godiste;
+            this.mestoRodjenja = mestoRodjenja;
+        }
+    }
 
-    b1 = new B(25);
-    b2 = new B();
 
 
-    window.A = A;
-    window.B = B;
+    OsobaOsobine.prototype = new Osoba();
+    OsobaOsobine.prototype.constructor = OsobaOsobine;
+
+    OsobaOsobine.prototype.godiste = 9999;
+    OsobaOsobine.prototype.mestoRodjenja = 'Nepoznato';
+
+    osoba3 = new OsobaOsobine("Djura", "Djuric");
+    //osoba3.godiste = 1991;
+    osoba3.mestoRodjenja = "Dete kafane";
+
+    osoba1 = new Osoba("Djura", "Djuric");
+    osoba2 = new Osoba();
+    //b1 = new B(25);
+    //b2 = new B();
+    window.Osoba= Osoba;
+    window.OsobaOsobine = OsobaOsobine;
+    //window.A = A;
+    //window.B = B;
+
+
 });
 
 // console.log(typeof undefined);
