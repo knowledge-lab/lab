ff333959fa71b707996864a8397f965c1ed5370b
Ivan Nikitovic <i.nikitovic@yahoo.ca>
2016-04-02 00:54:14 +0200

    ~

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 node_modules/.bin/grunt less
 
-node_modules/.bin/http-server -p $OPENSHIFTzz_NOzzDEJS_PORT -a $OPENSHIFT_NODEJS_IP
+node_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
