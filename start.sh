2251db7aaa9d7222f93a8c7c23a79c15de83fe9d
Ivan Nikitovic <i.nikitovic@yahoo.ca>
2016-04-01 23:31:30 +0200

    sadsad

--- start.sh
+++ start.sh
@@ -5,4 +5,3 @@ node_modules/.bin/bower install
 node_modules/.bin/grunt less
 
 node_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
-node_modules/.bin/json-server db.json
