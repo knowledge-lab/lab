de7a7b1577070181fc881bd7a9d1d08333fcc378
mugen <npavlovic101@gmail.com>
2016-04-02 01:23:31 +0200

    remove useless env

--- app/config.js
+++ app/config.js
@@ -1,20 +1,11 @@
-var ENVIRONMENTS = {
-    dev: '',
-    live: 'dist/'
-};
-
-var env = 'live';
-
 var require = {
     baseUrl: './',
 
     paths: (function() {
-        var root = ENVIRONMENTS[env];
-
         return {
-            application: root + 'app/application',
-            page: root + 'app/page',
-            model: root + 'app/model',
+            application: 'dist/app/application',
+            page: 'dist/app/page',
+            model: 'dist/app/model',
 
             text: 'lib/text/text',
             stache: "lib/requirejs-canjs-templates/stache",
@@ -23,7 +14,7 @@ var require = {
             can: "lib/CanJS/amd/can",
 
             'validate-lib': 'lib/validate/validate',
-            validate: root + 'app/lib/validate/validate'
+            validate: 'dist/app/lib/validate/validate'
         };
 
     })(),
