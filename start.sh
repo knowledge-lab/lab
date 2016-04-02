a83ca5b09b028d82bb4fc578bf6677b7632f629c
none <none@localhost.localdomain>
2016-04-02 07:16:01 -0400

    n missing

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo radim
 
 node_modules/.bin/grunt less
 
-ode_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
+node_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
