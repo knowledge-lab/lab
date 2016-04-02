3bb5826d59b2dd0205c49d5efdcce071e5690184
Ivan Nikitovic <ivan.nikitovic@shiftplanning.com>
2016-04-02 13:44:01 +0200

    minification

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo radim
-
 node_modules/.bin/grunt less
 
-ode_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
+node_modules/.bin/http-server -p $OPENSHIFT_NODEJS_PORT -a $OPENSHIFT_NODEJS_IP
