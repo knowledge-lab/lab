e3bf72a7cb65d433b53bde86dac118015e396ce4
Duki Duki <Duki Duki>
2016-04-04 13:15:57 +0200

    dodata moment.js biblioteka i dodata funkcionalnost brisanja Schedule entiteta klikom na kanticu

--- app/page/schedule/List/List.js
+++ app/page/schedule/List/List.js
@@ -10,34 +10,73 @@ define([
 ], function (Page, can, content, Schedule) {
 	return Page.extend(
 		{
-
+			defaults: {
+				
+				template: content
+			}
 		},
 		{
-			template : content,
 
-			init: function (element) {
-                var isLoading = can.compute(true);
+			init: function (element, options) {
 
-                var promise = Schedule.findAll({
+				this.modelView = new can.Map(
+					{
+						schedule:null,
+						isLoading: true
+					}
+				);
+
+				element.html(options.template(this.modelView));
+
+				this.setSchedules();
+
+            },
+
+            setSchedules: function () {
+            	var promise = Schedule.findAll({
 	                /**
 	                 * Saljemo na server _expand parametar kako bismo dobili i objekat menija u samom schedule objektu
 	                 */
 	                '_expand' : 'menu'
-                });
+                }); 
 
-                promise.always(
+                var that = this;
+
+				promise.always(
                     function () {
-                        isLoading(false);
+                        that.modelView.attr('isLoading', false);
                     }
                 );
+				this.modelView.attr('schedule', new Schedule.List(promise));
 
-                element.html(
-                    this.template({
-                        schedule: new Schedule.List(promise),
-                        isLoading: isLoading
-                    })
-                );
-            }
+				/**
+				 * Omogucavamo uvezivanje metoda
+				 */
+				return this;
+			},
+
+            removeSchedule: function(link, event){
+     
+            	event.preventDefault();
+            	var that = this;
+
+            	Schedule.destroy(event.target.id).then(
+            		function (){
+            			that.setSchedules();
+            		});
+            },
+
+            getSchedule: function () {
+				return this.modelView.attr('schedule');
+			},
+
+            '[data-action="remove-schedule"] click': 'removeSchedule'
+
+
+
+
+
+
 		}
 	);
 });
